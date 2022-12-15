35a37eea2c7b13cb1b72f245fec87baa3cefb5e1
olawale <waleshogbaike@yahoo.com>
2022-12-15 11:36:05 -0800

    task update

--- list_funcs2.c
+++ list_funcs2.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
-* AUTH: MUNYORODEE & BENNYARUZ 
+* AUTH: MUNYORODEE & BENNYARUZ
 * insert_dnodeint_at_index - inserts a node at a given index
 * in a doubly linked list
 * @h: double pointer to the list
